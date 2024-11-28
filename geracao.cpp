97bd21d422b834cb6fc38ba7f1178bd96267f0b7
uno0040 <montanhauno0040@gmail.com>
2024-11-28 14:30:41 -0300

    ADD: Geração de código

---
+++ geracao.cpp
@@ -0,0 +1,189 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include "geracao.hpp"
+
+using namespace std;
+
+// vou colocar aqui ta vendo
+
+void limpar_arquivo() {
+    ofstream arquivo("saida.txt", ios::trunc); // Abre o arquivo em modo truncado
+}
+
+// esta função checa se o arquivo existe
+inline bool arquivo_existe () {
+    ifstream f("saida.txt");
+    return f.good();
+}
+
+void geraLDC(int val){
+    ofstream arquivo("saida.txt", ios::app);
+    arquivo << "    " << left << setw(8) << "LDC" << val << "\n";
+    arquivo.close();
+}
+
+void geraLDV(int val){
+    ofstream arquivo("saida.txt", ios::app);
+    arquivo << "    " << left << setw(8) << "LDV" << val << "\n";
+    arquivo.close();
+}
+
+void geraADD() {
+    ofstream arquivo("saida.txt", ios::app);
+    arquivo << "    " << "ADD" << "\n";
+    arquivo.close();
+}
+
+void geraSUB() {
+    ofstream arquivo("saida.txt", ios::app);
+    arquivo << "    " << "SUB" << "\n";
+    arquivo.close();
+}
+
+void geraMULT() {
+    ofstream arquivo("saida.txt", ios::app);
+    arquivo << "    " << "MULT" << "\n";
+    arquivo.close();
+}
+
+void geraDIVI() {
+    ofstream arquivo("saida.txt", ios::app);
+    arquivo << "    " << "DIVI" << "\n";
+    arquivo.close();
+}
+
+void geraINV() {
+    ofstream arquivo("saida.txt", ios::app);
+    arquivo << "    " << "INV" << "\n";
+    arquivo.close();
+}
+
+void geraAND() {
+    ofstream arquivo("saida.txt", ios::app);
+    arquivo << "    " << "AND" << "\n";
+    arquivo.close();
+}
+
+void geraOR() {
+    ofstream arquivo("saida.txt", ios::app);
+    arquivo << "    " << "OR" << "\n";
+    arquivo.close();
+}
+
+void geraNEG() {
+    ofstream arquivo("saida.txt", ios::app);
+    arquivo << "    " << "NEG" << "\n";
+    arquivo.close();
+}
+
+void geraCME() {
+    ofstream arquivo("saida.txt", ios::app);
+    arquivo << "    " << "CME" << "\n";
+    arquivo.close();
+}
+
+void geraCMA() {
+    ofstream arquivo("saida.txt", ios::app);
+    arquivo << "    " << "CMA" << "\n";
+    arquivo.close();
+}
+
+void geraCEQ() {
+    ofstream arquivo("saida.txt", ios::app);
+    arquivo << "    " << "CEQ" << "\n";
+    arquivo.close();
+}
+
+void geraCDIF() {
+    ofstream arquivo("saida.txt", ios::app);
+    arquivo << "    " << "CDIF" << "\n";
+    arquivo.close();
+}
+
+void geraCMEQ() {
+    ofstream arquivo("saida.txt", ios::app);
+    arquivo << "    " << "CMEQ" << "\n";
+    arquivo.close();
+}
+
+void geraCMAQ() {
+    ofstream arquivo("saida.txt", ios::app);
+    arquivo << "    " << "CMAQ" << "\n";
+    arquivo.close();
+}
+
+void geraSTR(int val){
+    ofstream arquivo("saida.txt", ios::app);
+    arquivo << "    " << left << setw(8) << "STR" << val << "\n";
+    arquivo.close();
+}
+
+void geraJMP(int val){
+    ofstream arquivo("saida.txt", ios::app);
+    arquivo << "    " << left << setw(8) << "JMP" << val << "\n";
+    arquivo.close();
+}
+
+void geraJMPF(int val){
+    ofstream arquivo("saida.txt", ios::app);
+    arquivo << "    " << left << setw(8) << "JMP" << val << "\n";
+    arquivo.close();
+}
+
+void geraNULL(int val) {
+    ofstream arquivo("saida.txt", ios::app);
+    arquivo << left << setw(4) << val << "NULL" << "\n";
+    arquivo.close();
+}
+
+void geraRD() {
+    ofstream arquivo("saida.txt", ios::app);
+    arquivo << "    " << "RD" << "\n";
+    arquivo.close();
+}
+
+void geraPRN() {
+    ofstream arquivo("saida.txt", ios::app);
+    arquivo << "    " << "PRN" << "\n";
+    arquivo.close();
+}
+
+void geraSTART() {
+    ofstream arquivo("saida.txt", ios::app);
+    arquivo << "    " << "START" << "\n";
+    arquivo.close();
+}
+
+void geraALLOC(int val, int val2){
+    ofstream arquivo("saida.txt", ios::app);
+    arquivo << "    " << left << setw(8) << "ALLOC" << val;
+    arquivo << setw(4) << right << setw(4) << val2 << "\n";
+    arquivo.close();
+}
+
+void geraDALLOC(int val, int val2){
+    ofstream arquivo("saida.txt", ios::app);
+    arquivo << "    " << left << setw(8) << "DALLOC" << val;
+    arquivo << setw(4) << right << setw(4) << val2 << "\n";
+    arquivo.close();
+}
+
+void geraHLT() {
+    ofstream arquivo("saida.txt", ios::app);
+    arquivo << "    " << "HLT" << "\n";
+    arquivo.close();
+}
+
+void geraCALL(int val) {
+    ofstream arquivo("saida.txt", ios::app);
+    arquivo << left << setw(4) << val << "CALL" << "\n";
+    arquivo.close();
+}
+
+void geraRETURN() {
+    ofstream arquivo("saida.txt", ios::app);
+    arquivo << "    " << "RETURN" << "\n";
+    arquivo.close();
+}
---
+++ geracao.hpp
@@ -0,0 +1,42 @@
+#ifndef GERACAO_HPP
+#define GERACAO_HPP
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include "sintatico.hpp"
+
+using namespace std;
+
+void limpar_arquivo();
+inline bool arquivo_existe();
+void geraLDC(int val);
+void geraLDV(int val);
+void geraADD();
+void geraSUB();
+void geraMULT();
+void geraDIVI();
+void geraINV();
+void geraAND();
+void geraOR();
+void geraNEG();
+void geraCME();
+void geraCMA();
+void geraCEQ();
+void geraCDIF();
+void geraCMEQ();
+void geraCMAQ();
+void geraSTR(int val);
+void geraJMP(int val);
+void geraJMPF(int val);
+void geraNULL(int val);
+void geraRD();
+void geraPRN();
+void geraSTART();
+void geraALLOC(int val, int val2);
+void geraDALLOC(int val, int val2);
+void geraHLT();
+void geraCALL(int val);
+void geraRETURN();
+
+#endif
